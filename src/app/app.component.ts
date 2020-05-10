1b997783a762ec4a0110122de15a6b959097fce6
sarival <sarival@sarival.com.br>
2020-05-10 14:09:36 -0300

    Criando 3 sradd 3 strings ao atributo 'todos'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public todos:any[] = [];
+  public todos: any[] = []; //tipando uma variavel - usar :
+
+  constructor() {
+    this.todos.push("estudar angular");
+    this.todos.push("ir ao suprmercado");
+    this.todos.push("cortar o cabelo");
+  }
 }
